89a6c0987b83e415aeb72b68d9dbd7f24075d479
Sebastien Stordeur <stordeursebastien0@gmail.com>
2021-08-09 15:34:25 +0200

    navbar + logo imported

--- src/Components/Headers/LoginSignup/Navbar.js
+++ src/Components/Headers/LoginSignup/Navbar.js
@@ -1,16 +1,29 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
-
+import "../../../style.css";
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <ul className="navbar__list">
-        <Link to="/register"><li>S'inscrire</li></Link>
-        <Link to="/signup"><li>Se connecter</li></Link>
-      </ul>
-    </nav>
-  )
-}
+    <header className="signup-header">
+      <nav className="navbar">
+        <ul className="navbar__list">
+          <Link to="/register">
+            <li>S'inscrire</li>
+          </Link>
+          <li className="navbar__logo">
+            <img
+              className="logo"
+              src="../../../images/icon.png"
+              alt="Groupomania Logo"
+            />
+          </li>
+          <Link to="/signup">
+            <li>Se connecter</li>
+          </Link>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
