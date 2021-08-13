70ed6989777810d3f1a85b78071a601ba79e4d40
Sebastien Stordeur <stordeursebastien0@gmail.com>
2021-08-13 15:41:10 +0200

    multer config to add image to a post

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,8 +1,17 @@
 const { Post } = require('../models/postModel');
-
+const fs = require('fs');
 
 exports.createPost = (req,res,next) => {
   
-  const post = new Post
-
+  const postObject = JSON.parse(req.body.post);
+  delete postObject.id;
+  const post = new Post({
+    ...body.postObject,
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+    likes: 0,
+    dislikes: 0,
+  });
+  post.save()
+    .then(() => res.status(201).JSON({ message: "Post crée" }))
+    .catch(error => res.status(400).JSON({ message: "Impossible de créer ce post" + error }));
 }
