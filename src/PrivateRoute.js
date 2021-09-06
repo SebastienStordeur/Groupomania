bcc36e9f2db6b561a004ab54130c182cd1469e3d
Sebastien Stordeur <stordeursebastien0@gmail.com>
2021-09-06 09:55:00 +0200

    public route, private route, restricted route + isLogin test function

---
+++ src/PrivateRoute.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Route, Redirect } from react-router-dom;
+import { isLogin } from './utils/index';
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  return (
+
+    //show the component only when the user is logged in
+    //Otherwise, redirect to register page
+    <Route { ...rest} render={ props => (
+      isLogin() ?
+        <Component { ...props } />
+      : <Redirect to="Register" />
+    )} />
+  )
+}
+
+export default PrivateRoute
---
+++ src/PublicRoute.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { isLogin } from './utils/index';
+
+const PublicRoute = ({ component: Component, restricted, ...rest }) => {
+  return (
+    // if restricted =  false
+    // = public route
+    // if restricted = true
+    // = restricted route
+    <Route { ...rest } render={ ...props => (
+      isLogin() && restricted ?
+        <Redirect to="/dashboard" />
+      : <Component { ...props } />
+    )} />
+  )
+}
+
+export default PublicRoute
